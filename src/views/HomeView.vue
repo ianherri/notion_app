79f0236b6fb57720c3e848d80aba7a790761fd11
Ian Herrington <ianherri20@gmail.com>
2022-07-07 11:56:47 -0500

    add cron to server/index but do not get it working yet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,21 @@
   <h1>Home</h1>
   <p>Cool stuff coming soon...</p>
   <p>Try navigating to the page list</p>
+
   <SendRandText />
+  <h3>Cron Options</h3>
+  <p>Select your text frequency below:</p>
+  <form class="cron-input">
+    <select id="time-interval" v-model="interval">
+      <option
+        v-for="option in cronIntervalOptions"
+        :key="option.id"
+        :value="option.interval"
+      >
+        {{ option.interval }}
+      </option>
+    </select>
+  </form>
 </template>
 
 <script>
@@ -10,7 +24,17 @@ import SendRandText from '@/components/SendRandText.vue'
 export default {
   components: { SendRandText },
   setup() {
-    return {}
+    const cronIntervalOptions = [
+      { id: 0, interval: '12 hours' },
+      { id: 1, interval: '24 hours' },
+      { id: 2, interval: '48 hours' },
+      { id: 3, interval: '72 hours' },
+    ]
+    const interval = cronIntervalOptions[0].interval
+    return {
+      interval,
+      cronIntervalOptions,
+    }
   },
 }
 </script>
